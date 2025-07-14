c044b0470476a15bb3fcd4417c8a915a37cbc98c
choijin <cjg030820@gmail.com>
2025-07-14 18:11:40 +0900

    my portfolio

---
+++ scripts/index.js
@@ -0,0 +1,37 @@
+const pageSlide = new Swiper('#wrap',{
+    direction:'vertical',
+    mousewheel:true,
+})
+const projectSwiper = new Swiper('.project_swiper',{
+    navigation:{
+        prevEl:'.project_swiper .swiper-button-prev',
+        nextEl:'.project_swiper .swiper-button-next',
+    },
+    scrollbar:{
+        el:'.project_swiper .swiper-scrollbar'
+    }
+})
+const designSwiper = new Swiper('.design_swiper',{
+    navigation:{
+        prevEl:'.design_swiper .swiper-button-prev',
+        nextEl:'.design_swiper .swiper-button-next',
+    },
+    scrollbar:{
+        el:'.design_swiper .swiper-scrollbar'
+    }
+})
+//네비게이션
+const nav = document.querySelectorAll('nav a');
+nav.forEach((obj,idx)=>{
+    console.log(obj, idx)
+    obj.addEventListener('click',(e)=>{
+        e.preventDefault();
+        pageSlide.slideTo(idx, 1000);
+    })
+})
+const skills = document.querySelectorAll('.skills ul li');
+for(let i = 0 ; i < 6 ; i++){
+    skills[i] = addEventListener('mouseover', ()=>{
+        
+    })
+}
