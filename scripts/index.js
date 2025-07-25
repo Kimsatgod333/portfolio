19bf48b1d14ce2e9ad6bbc566d2691b021e47c07
choijin <cjg030820@gmail.com>
2025-07-25 19:18:17 +0900

    디자인 수정

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
 const pageSlide = new Swiper('#wrap',{
     direction:'vertical',
     mousewheel:true,
+    keyboard: {
+        enabled: true,
+    },
 })
 const projectSwiper = new Swiper('.project_swiper',{
     navigation:{
@@ -14,12 +17,21 @@ const projectSwiper = new Swiper('.project_swiper',{
     pagination:{
         el:'.project .slide_btn_wrap .swiper-pagination',
         type:'fraction',
-    }
+    },
+    loop:true,
+    keyboard: {
+        enabled: true,
+    },
 })
-const designSwiper = new Swiper('.design_swiper',{
-    slidesPerView:4,
-    autoplay:{delay:0},
-    speed:4000,
+const bnrSwiper = new Swiper('.design_bnr_swiper',{
+    slidesPerView:3,
+    spaceBetween:40,
+})
+const widthSwiper = new Swiper('.design_width_bnr_swiper',{
+
+})
+const detailsSwiper = new Swiper('.design_details_swiper',{
+    direction:'vertical'
 })
 //네비게이션
 const nav = document.querySelectorAll('nav a');
